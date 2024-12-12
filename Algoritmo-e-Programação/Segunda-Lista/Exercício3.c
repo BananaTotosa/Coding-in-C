378e5e6f39d07ad43f0900a3b57144fc14ccd0ca
Vinicius Oliveira <102190811+ViniciusHenriqueOliveira@users.noreply.github.com>
2024-12-12 17:43:21 -0300

    Create Exercício3.c

---
+++ Algoritmo-e-Programação/Segunda-Lista/Exercício3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	float n1, n2;
+	
+	printf("Digite os numeros: ");
+	scanf("%f %f", &n1, &n2);
+	
+	if (n1 < n2)
+	printf("n1 < n2");
+	else 
+	printf("n2 < n1");
+	
+	return 0;
+}
