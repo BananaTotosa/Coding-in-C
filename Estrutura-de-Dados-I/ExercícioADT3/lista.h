4fd6b1eb98c13c07a6d40d705e0772ec96ca1cef
Vinicius Oliveira <102190811+ViniciusHenriqueOliveira@users.noreply.github.com>
2024-12-14 23:58:20 -0300

    Create lista.h

---
+++ Estrutura-de-Dados-I/ExercícioADT3/lista.h
@@ -0,0 +1,12 @@
+typedef struct lista Lista;
+
+Lista* criarLista();
+void inserir(Lista* lista, int valor);
+void inserirEm(Lista* lista, int posicao, int valor);
+int alterar(Lista* lista, int posicao, int valor);
+int remover(Lista* lista, int posicao);
+void imprimir(Lista* lista);
+int quantidadeItens(Lista* lista);
+int estaVazia(Lista* lista);
+int buscar(Lista* lista, int valor);
+void liberarMemoria(Lista* lista);
